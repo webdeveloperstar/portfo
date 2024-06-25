0e4673e63fcd6ec7aa9f776738d16de41c5cb706
Gulam Rasool Khan <145851474+webdeveloperstar@users.noreply.github.com>
2024-06-25 19:33:38 +0530

    Add files via upload
    Awesome portfolio responsive website...

---
+++ script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+const sr = ScrollReveal ({
+    distance: '80px',
+    durtation: 2800,
+    reset: true
+})
+
+sr.reveal('.text,.social',{delay:300, origin: 'bottom'})
+sr.reveal('header',{delay:100, origin: 'top'})
